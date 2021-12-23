dc1794c6bea8e3109c79fc223b9085fa967a7e6d
saba <moslehsaba@gmail.com>
2021-12-23 20:47:17 +0330

    initial commit

---
+++ src/test/java/com/levelup/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelup.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
